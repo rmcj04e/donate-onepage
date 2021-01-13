3df6693fcfeaf5502a81b69c1f623c478535dc86
RuMineCheats <rovio.org@gmail.com>
2021-01-13 18:36:01 +0300

    Added donate by server

---
+++ src/views/Servers.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <b-row>
+<div v-for="server in servers" :key="server.id">
+    <b-col>
+<b-card bg-variant="dark" text-variant="white"
+    :title="server.name"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      {{server.desc}}
+    </b-card-text>
+
+    <router-link :to="{ name: 'donlist', params:{ id:server.name } }"><b-button variant="primary">Список донатов</b-button></router-link>
+  </b-card>
+    </b-col>
+          </div>
+      </b-row>
+  </div>
+</template>
+
+<script>
+import axios from `axios`
+export default{
+    name:"Servers",
+    data(){
+        return{
+           servers: []
+        }
+    },
+    mounted(){
+      axios
+        .get("/api/rest.api?action=get_servers")
+        .then((response) => {
+            this.servers = response.data
+        })
+        .catch(() => {
+            console.log("An error has happened")
+        })
+    }
+}
+</script>
